f7d532daf8efe97ec2ea3bea5e2d52b3d4717041
Vittor Fonseca Serra <vittorserradev@gmail.com>
2023-01-25 22:29:16 -0300

    versão 1.6

--- 04-Type Casting/4.2-Explicit Type Casting/Explicit Type Casting.js
+++ 04-Type Casting/4.2-Explicit Type Casting/Explicit Type Casting.js
@@ -1,3 +1,10 @@
 /*Converter um tipo de dados para outro tipo de dados, ou seja, a conversão de um tipo de dados string para 
 booleano ou a conversão de um tipo de dados inteiro para tipo de dados string. O typecasting em JavaScript 
 também é conhecido como conversão de tipo ou coerção de tipo.*/
+
+let number = 10;
+
+console.log(number);
+console.log(typeof(number));
+console.log(number.toString());
+console.log(typeof number.toString());
